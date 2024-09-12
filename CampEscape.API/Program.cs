21747652900156cc87324d292598342ad1f99946
johnweaver <johnweaver@zohomail.com>
2024-09-12 15:11:40 -0700

    added serilog for logging

--- CampEscape.API/Program.cs
+++ CampEscape.API/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
         builder.AllowAnyOrigin();
         builder.AllowAnyHeader();
         builder.AllowAnyMethod();
-    });      
+    });
 });
 
 builder.Host.UseSerilog((context, logConfig) =>
---
+++ CampEscapeAPI/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CampEscapeAPI.Controllers
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestController
+    {
+        [HttpGet]
+        public List<string> GetStrings()
+        {
+            var listTest = new List<string>
+            {
+                "one",
+                "two"
+            };
+            return listTest; 
+        }
+    }
+}
